907d27a6a66c79498cad79cd98fb6a31e1a65641
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-02 03:07:08 +0800

    save

--- PressToBuy.ts
+++ PressToBuy.ts
@@ -1,6 +1,13 @@
+/**
+ * @description Press to buy code blocks.
+ */
+
 const id = "forest";
+
 const cost = 16;
+
 const phase = globalThis.phasesByIds.get(id);
+
 if (phase) {
   if (api.hasItem(playerId, "Gold Coin")) {
     const amount = api.getInventoryItemAmount(playerId, "Gold Coin");
@@ -17,4 +24,6 @@ if (phase) {
   } else {
     m(playerId, `You need ${cost} gold coin to buy ${phase.name}.`, ms("red"));
   }
+} else {
+  m(playerId, `Internal error: item not found.`, ms("red"));
 }
