6fea063692effea94e561042b6d30387925b662c
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-02 02:52:59 +0800

    save

--- PressToBuy.ts
+++ PressToBuy.ts
@@ -1,18 +1,20 @@
-const phase = globalThis.phasesByIds.get("forest");
+const id = "forest";
+const cost = 16;
+const phase = globalThis.phasesByIds.get(id);
 if (phase) {
   if (api.hasItem(playerId, "Gold Coin")) {
     const amount = api.getInventoryItemAmount(playerId, "Gold Coin");
-    if (amount >= 125) {
-      api.removeItemName(playerId, "Gold Coin", 125);
+    if (amount >= cost) {
+      api.removeItemName(playerId, "Gold Coin", cost);
       api.giveItem(playerId, "Stick", 1, {
         customDisplayName: phase.name,
         customDescription: phase.description,
       });
       m(playerId, `You received ${phase.name}.`, ms("gold"));
     } else {
-      m(playerId, `You need 125 gold coin to buy ${phase.name}.`, ms("red"));
+      m(playerId, `You need cost gold coin to buy ${phase.name}.`, ms("red"));
     }
   } else {
-    m(playerId, `You need 125 gold coin to buy ${phase.name}.`, ms("red"));
+    m(playerId, `You need cost gold coin to buy ${phase.name}.`, ms("red"));
   }
 }
