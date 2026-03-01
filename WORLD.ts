8268f7047f08820245d402d8d51d6747c1b205ca
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-01 14:39:25 +0800

    save

--- WORLD.ts
+++ WORLD.ts
@@ -1,7 +1,7 @@
 type GenericFunction = (...args: any[]) => any;
 
-declare const api:{
-	[key:string]: GenericFunction
+declare const api: {
+	[key: string]: GenericFunction
 }
 
 declare let tick: GenericFunction
@@ -68,8 +68,8 @@ declare let onPlayerFinishQTE: GenericFunction
 declare let onPlayerBoughtShopItem: GenericFunction
 declare let doPeriodicSave: GenericFunction
 
-class MyStorage {
-	static init (playerId: any, x: any, y: any, z: any) {
+class ChestStorage {
+	static init(playerId: any, x: any, y: any, z: any) {
 		if (api.getBlock(x, y, z) === "Air") {
 			api.setBlock(x, y, z, "Chest");
 			api.setStandardChestItemSlot(
@@ -87,7 +87,7 @@ class MyStorage {
 		}
 		return false;
 	}
-	static isStorage (x: any, y: any, z: any) {
+	static isStorage(x: any, y: any, z: any) {
 		if (api.getBlock(x, y, z) === "Chest") {
 			const item = api.getStandardChestItemSlot(
 				[x, y, z],
@@ -100,7 +100,7 @@ class MyStorage {
 		}
 		return false;
 	}
-	static isOwnStorage (playerId: any, x: any, y: any, z: any) {
+	static isOwnStorage(playerId: any, x: any, y: any, z: any) {
 		if (api.getBlock(x, y, z) === "Chest") {
 			const item = api.getStandardChestItemSlot(
 				[x, y, z],
@@ -116,8 +116,8 @@ class MyStorage {
 		}
 		return false;
 	}
-	static set (playerId: any, x: any, y: any, z: any, index: number, value: any) {
-		if (MyStorage.isStorage(x, y, z) && 0 < index && index <= 35) {
+	static set(playerId: any, x: any, y: any, z: any, index: number, value: any) {
+		if (ChestStorage.isStorage(x, y, z) && 0 < index && index <= 35) {
 			api.setStandardChestItemSlot(
 				[x, y, z],
 				index,
@@ -134,8 +134,8 @@ class MyStorage {
 			);
 		}
 	}
-	static get (playerId: any, x: any, y: any, z: any, index: number) {
-		if (MyStorage.isStorage(x, y, z) && 0 < index && index <= 35) {
+	static get(playerId: any, x: any, y: any, z: any, index: number) {
+		if (ChestStorage.isStorage(x, y, z) && 0 < index && index <= 35) {
 			const item = api.getStandardChestItemSlot(
 				[x, y, z],
 				index,
@@ -152,8 +152,8 @@ class MyStorage {
 		}
 		return null;
 	}
-	static teardown (playerId: any, x: any, y: any, z: any) {
-		if (MyStorage.isStorage(x, y, z)) {
+	static teardown(playerId: any, x: any, y: any, z: any) {
+		if (ChestStorage.isStorage(x, y, z)) {
 			for (let index = 0; index <= 35; index += 1) {
 				api.setStandardChestItemSlot(
 					[x, y, z],
@@ -166,13 +166,13 @@ class MyStorage {
 			}
 		}
 	}
-  static onPlayerAttemptOpenChest = (playerId: any, x: any, y: any, z: any,	_isMoonstoneChest: any, _isIronChest: any) => {
-    if (MyStorage.isStorage(x, y, z)) {
-      api.sendMessage(playerId, "You can't open that.", { color: "gold" });
-      return "preventOpen";
-    }
-    return undefined;
-  }
+	static onPlayerAttemptOpenChest = (playerId: any, x: any, y: any, z: any, _isMoonstoneChest: any, _isIronChest: any) => {
+		if (ChestStorage.isStorage(x, y, z)) {
+			api.sendMessage(playerId, "You can't open that.", { color: "gold" });
+			return "preventOpen";
+		}
+		return undefined;
+	}
 }
 
 type PhaseBlock = [string, number];
@@ -182,8 +182,10 @@ interface Phase {
 	blocks: PhaseBlock[];
 }
 
+const phases = new Map<string, Phase>();
+
 const plains: Phase = {
-	name: "Plains",
+	name: "One Block (Plains)",
 	blocks: [
 		["Grass Block", 128],
 		["Messy Stone", 128],
@@ -193,14 +195,15 @@ const plains: Phase = {
 		["Diamond Ore", 8],
 	],
 };
+phases.set(plains.name, plains);
 
-class OneBlock{
-	static randomInt (min: number, max: number) {
+class OneBlock {
+	static randomInt(min: number, max: number) {
 		const minc = Math.ceil(min);
 		const maxf = Math.floor(max);
 		return Math.floor(Math.random() * (maxf - minc + 1) + minc);
 	}
-	static getRandomBlock (blocks: PhaseBlock[]): PhaseBlock {
+	static getRandomBlock(blocks: PhaseBlock[]): PhaseBlock {
 		const total = blocks.reduce((acc: any, block: any[]) => acc + block[1], 0);
 		const random = OneBlock.randomInt(1, total);
 		let sum = 0;
@@ -210,127 +213,139 @@ class OneBlock{
 				return block;
 			}
 		}
-    return ["Air", 0];
+		return ["Air", 0];
 	}
 
-  static onPlayerChangeBlock (playerId: any, x: any, y: number, z: any, fromBlock: string, toBlock: string, _droppedItem: any, _fromBlockInfo: any, _toBlockInfo: any) {
-    if (fromBlock === "Air") {
-      const held = api.getHeldItem(playerId);
-      if (held?.attributes?.customDisplayName === "One Block (Plains)") {
-        const above = api.getBlock(x, y + 1, z);
-        if (above === "Air") {
-          MyStorage.init(playerId, x, y, z);
-          const block = OneBlock.getRandomBlock(plains.blocks);
-          api.setBlock(x, y + 1, z, block[0]);
-          MyStorage.set(playerId, x, y, z, 1, "one_block_plains");
-        } else {
-          api.sendMessage(
-            playerId,
-            "Invalid placement.",
-            { color: "gold" },
-          );
-          return "preventChange";
-        }
-      }
-    }
-    if (toBlock === "Air") {
-      if (MyStorage.isStorage(x, y, z)) {
-        if (MyStorage.isOwnStorage(playerId, x, y, z)) {
-          if (MyStorage.get(playerId, x, y, z, 1) === "one_block_plains") {
-            MyStorage.teardown(playerId, x, y, z);
-            api.setBlock(x, y + 1, z, "Air");
-            api.giveItem(playerId, "Chest", 1, {
-              customDisplayName: "One Block (Plains)",
-              customDescription: "Spawns plains blocks on top of it!",
-            });
-            return "preventDrop";
-          }
-        } else {
-          return "preventChange";
-        }
-      }
-      if (MyStorage.isStorage(x, y - 1, z)) {
-        if (MyStorage.get(playerId, x, y - 1, z, 1) === "one_block_plains") {
-                const block = OneBlock.getRandomBlock(plains.blocks);
-          api.setBlock(x, y, z, block[0]);
-        }
-      }
-    }
-    return undefined;
-  }
+	static onPlayerChangeBlock(playerId: any, x: any, y: number, z: any, fromBlock: string, toBlock: string, _droppedItem: any, _fromBlockInfo: any, _toBlockInfo: any) {
+		/**
+		 * @description One Block Placement
+		 */
+		if (fromBlock === "Air") {
+			const held = api.getHeldItem(playerId);
+			if (held?.attributes?.customDisplayName === "One Block (Plains)") {
+				const above = api.getBlock(x, y + 1, z);
+				if (above === "Air") {
+					ChestStorage.init(playerId, x, y, z);
+					const block = OneBlock.getRandomBlock(plains.blocks);
+					api.setBlock(x, y + 1, z, block[0]);
+					ChestStorage.set(playerId, x, y, z, 1, "one_block_plains");
+				} else {
+					api.sendMessage(
+						playerId,
+						"Invalid placement.",
+						{ color: "gold" },
+					);
+					return "preventChange";
+				}
+			}
+		}
+		/**
+		 * @description Displacement
+		 */
+		if (toBlock === "Air") {
+			/**
+			 * @description One Block Displacement
+			 */
+			if (ChestStorage.isStorage(x, y, z)) {
+				if (ChestStorage.isOwnStorage(playerId, x, y, z)) {
+					if (ChestStorage.get(playerId, x, y, z, 1) === "one_block_plains") {
+						ChestStorage.teardown(playerId, x, y, z);
+						api.setBlock(x, y + 1, z, "Air");
+						api.giveItem(playerId, "Chest", 1, {
+							customDisplayName: "One Block (Plains)",
+							customDescription: "Spawns plains blocks on top of it!",
+						});
+						return "preventDrop";
+					}
+				} else {
+					return "preventChange";
+				}
+			}
+			/**
+			 * @description Block Displacement
+			 */
+			if (ChestStorage.isStorage(x, y - 1, z)) {
+				if (ChestStorage.get(playerId, x, y - 1, z, 1) === "one_block_plains") {
+					const block = OneBlock.getRandomBlock(plains.blocks);
+					api.setBlock(x, y, z, block[0]);
+				}
+			}
+		}
+		return undefined;
+	}
 
-  static onPlayerChat (playerId: any, chatMessage: any) {
-    switch (chatMessage) {
-      case ".plains": {
-        api.giveItem(playerId, "Chest", 1, {
-          customDisplayName: "One Block (Plains)",
-          customDescription: "Spawns plains blocks on top of it!",
-          customAttributes: {}
-        });
-        api.sendMessage(
-          playerId,
-          "You received One Block (Plains).",
-          { color: "gold" },
-        );
-        return false;
-      }
-      default: {
-        break;
-      }
-    }
-    return undefined;
-  }
+	static onPlayerChat(playerId: any, chatMessage: any) {
+		switch (chatMessage) {
+			case ".plains": {
+				api.giveItem(playerId, "Chest", 1, {
+					customDisplayName: "One Block (Plains)",
+					customDescription: "Spawns plains blocks on top of it!",
+					customAttributes: {}
+				});
+				api.sendMessage(
+					playerId,
+					"You received One Block (Plains).",
+					{ color: "gold" },
+				);
+				return false;
+			}
+			default: {
+				break;
+			}
+		}
+		return undefined;
+	}
 }
 
 class TownSquare {
-  static onPlayerJoin (playerId: any) {
-    api.setCantChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64]);
-  }
-  static onPlayerDamagingOtherPlayer (attackingPlayer: any, damagedPlayer: any) {
-    if (api.isInsideRect(api.getPosition(damagedPlayer), [-64, -1024, -64], [64, 1024, 64])) {
-      api.sendMessage(attackingPlayer, "Can't attack inside the town square.", { color: "gold" });
-      return "preventDamage";
-    }
-    return undefined;
-  }
-  static onPlayerChat (playerId: any, chatMessage: any) {
-    switch (chatMessage) {
-      case ".lock": {
-        api.setCantChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64]);
-        api.sendMessage(playerId, "Locked spawn area.", { color: "gold" });
-        return false;
-      }
-      case ".unlock": {
-        api.setCanChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64] );
-        api.sendMessage(playerId, "Unlocked spawn area.", { color: "gold" });
-        return false;
-      }
-      default: {
-        break;
-      }
-    }
-    return undefined
-  }
+	static onPlayerJoin(playerId: any) {
+		api.setCantChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64]);
+	}
+	static onPlayerDamagingOtherPlayer(attackingPlayer: any, damagedPlayer: any) {
+		if (api.isInsideRect(api.getPosition(damagedPlayer), [-64, -1024, -64], [64, 1024, 64])) {
+			api.sendMessage(attackingPlayer, "Can't attack inside the town square.", { color: "gold" });
+			return "preventDamage";
+		}
+		return undefined;
+	}
+	static onPlayerChat(playerId: any, chatMessage: any) {
+		switch (chatMessage) {
+			case ".lock": {
+				api.setCantChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64]);
+				api.sendMessage(playerId, "Locked spawn area.", { color: "gold" });
+				return false;
+			}
+			case ".unlock": {
+				api.setCanChangeBlockRect(playerId, [-64, -1024, -64], [64, 1024, 64]);
+				api.sendMessage(playerId, "Unlocked spawn area.", { color: "gold" });
+				return false;
+			}
+			default: {
+				break;
+			}
+		}
+		return undefined
+	}
 }
 
 onPlayerJoin = (playerId: any) => {
-  return TownSquare.onPlayerJoin(playerId);
+	return TownSquare.onPlayerJoin(playerId);
 };
 
 onPlayerChangeBlock = (playerId: any, x: any, y: number, z: any, fromBlock: string, toBlock: string, droppedItem: any, fromBlockInfo: any, toBlockInfo: any) => {
-  return OneBlock.onPlayerChangeBlock(playerId, x, y, z, fromBlock, toBlock, droppedItem, fromBlockInfo, toBlockInfo);
+	return OneBlock.onPlayerChangeBlock(playerId, x, y, z, fromBlock, toBlock, droppedItem, fromBlockInfo, toBlockInfo);
 };
 
 onPlayerDamagingOtherPlayer = (attackingPlayer: any, damagedPlayer: any) => {
-  return TownSquare.onPlayerDamagingOtherPlayer(attackingPlayer, damagedPlayer);
+	return TownSquare.onPlayerDamagingOtherPlayer(attackingPlayer, damagedPlayer);
 };
 
 onPlayerChat = (playerId: any, chatMessage: any) => {
 	return TownSquare.onPlayerChat(playerId, chatMessage) ?? OneBlock.onPlayerChat(playerId, chatMessage);
 }
 
-onPlayerAttemptOpenChest = (playerId: any, x: any, y: any, z: any,	isMoonstoneChest: any, isIronChest: any) => {
-  return MyStorage.onPlayerAttemptOpenChest(playerId, x, y, z,	isMoonstoneChest, isIronChest)
+onPlayerAttemptOpenChest = (playerId: any, x: any, y: any, z: any, isMoonstoneChest: any, isIronChest: any) => {
+	return ChestStorage.onPlayerAttemptOpenChest(playerId, x, y, z, isMoonstoneChest, isIronChest)
 };
 
 
