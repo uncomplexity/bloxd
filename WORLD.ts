54eceac10f705f6ecae51ec039c4a7dab5b0af97
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-03 01:28:42 +0800

    save

--- WORLD.ts
+++ WORLD.ts
@@ -564,23 +564,23 @@ class ProtectedChunks {
 		const chestId = api.blockNameToBlockId("Chest");
 		const chunkPosition = api.chunkIdToBotLeftCoord(chunkId);
 		const chunkData = api.getChunk(chunkPosition);
-		b(JSON.stringify(chunkPosition), s("gold"));
-		b(JSON.stringify(chunkData), s("gold"));
-    for (let x = 0; x < 32; x++) {
-        for (let y = 0; y < 32; y++) {
-            for (let z = 0; z < 32; z++) {
-                const id = chunkData.blockData.get(x, y, z);
-                if (id === chestId) {
-                    const worldX = chunkPosition[0] + x;
-                    const worldY = chunkPosition[1] + y;
-                    const worldZ = chunkPosition[2] + z;
-                    if (ChestStorage.isStorage(worldX, worldY, worldZ)) {
-											b(`Chest Storage found at ${worldX}, ${worldY}, ${worldZ}.`, s("gold"));
-                    }
-                }
-            }
-        }
-    }
+		if (chunkData) {
+			for (let x = 0; x < 32; x++) {
+					for (let y = 0; y < 32; y++) {
+							for (let z = 0; z < 32; z++) {
+									const id = chunkData.blockData.get(x, y, z);
+									if (id === chestId) {
+											const worldX = chunkPosition[0] + x;
+											const worldY = chunkPosition[1] + y;
+											const worldZ = chunkPosition[2] + z;
+											if (ChestStorage.isStorage(worldX, worldY, worldZ)) {
+												b(`Chest Storage found at ${worldX}, ${worldY}, ${worldZ}.`, s("gold"));
+											}
+									}
+							}
+					}
+			}
+		}
 	}
 }
 
